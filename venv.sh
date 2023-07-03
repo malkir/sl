891b28eb9f0d98402f9c9464f761f848808e2ff4
Daniel Collins <malkir@gmail.com>
2023-07-02 23:42:11 -0700

    Poetry baby

--- venv.sh
+++ venv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-GIT_DIR="/home/${USER}/git/slcli"
+# Source this file to initialize the venv and install deps
+# To generate a dev requirements, run:
+#   pip-compile requirements.in -o requirements.txt
+GIT_DIR="/home/${USER}/git/slapi"
 VENV_DIR="${GIT_DIR}/slvenv"
 VENV_ACTIVATE="${VENV_DIR}/bin/activate"
 HOST_PYTHON=`which python3`
@@ -14,6 +17,6 @@ if [[ -f ${VENV_ACTIVATE} ]]; then
   set +x
   echo "Activating ${VENV_ACTIVATE}"
   source ${VENV_ACTIVATE}
-  ./slvenv/bin/python3 -m pip install -r requirements.txt
+  ./slvenv/bin/python3 -m pip insall poetry
 fi
 
